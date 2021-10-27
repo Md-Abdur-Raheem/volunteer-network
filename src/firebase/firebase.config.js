5ce8b99a6a8783e08e0c22930aa5b0856be74876
Md-Abdur-Raheem <md.raheem2450@gmail.com>
2021-10-27 16:29:35 +0600

    firebase config updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,8 +1,10 @@
-import { initializeApp } from "firebase/app";
-import firebaseConfig from "./firebase.init"
-
-const initAuthentication = () => {
-    initializeApp(firebaseConfig)
-}
-
-export default initAuthentication;
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENGER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+  
+export default firebaseConfig;
